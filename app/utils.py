08e6a93d3e4ac3db5d1381e252471a781153e0a8
sea-kelp <66500457+sea-kelp@users.noreply.github.com>
2022-05-12 17:28:39 -0700

    Fix CCS URL regex

--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,7 @@ import re
 class Regexps:
   CASE_NUM = "\\d{4}OPA-\\d{4}"
   CCS_DISPOSITION = "((No|All) Allegations Sustained|Partially Sustained|-)"
-  CCS_URL = "https://www.seattle.gov/Documents/Departments/OPA/ClosedCaseSummaries/\\d{4}OPA-\\d{4}ccs\\d{6}.pdf"
+  CCS_URL = "https://www.seattle.gov/Documents/Departments/OPA/ClosedCaseSummaries/\\d{4}OPA-\\d{4}ccs\\d{4,10}.pdf"
   SERIAL = "\\d{1,4}"
   STRING = "[\\w \\-,]{1,255}"
   TIMESTAMP = "\\d{4}(-\\d{2}){2}T\\d{2}(:\\d{2}){2}(.\\d{3})?"
